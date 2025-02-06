1312ab4fe6ddced4260715fc4a9c5bf4e003c75e
akarshbharatha <bharathaakarsh@gmail.com>
2025-02-06 12:31:07 +0530

    Printing 100 Numbers

--- Demo004.java
+++ Demo004.java
@@ -2,7 +2,7 @@ public class Demo004 {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+            System.out.println(" Number " + i);
         }
     }
 }
