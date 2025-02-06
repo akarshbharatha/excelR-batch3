f5f3201641f5b55ad0d0658714d5e6e3e6efff5b
akarshbharatha <bharathaakarsh@gmail.com>
2025-02-06 12:37:08 +0530

    Displaying Account Details Using Another claas

---
+++ Demo010.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Demo010 {
+    public static void main(String[] args) {
+        Customer obj = new Customer();
+        obj.accept_details();
+        obj.Display_details();
+    }
+}
